7098736a5145f8b59d4ff3eb9a1162f3da6bbd25
Ahraf01096 <ahmed.ashraf27163@gmail.com>
2024-07-31 11:07:10 +0300

    add setup Service Locator to our app

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,20 @@
-import 'package:bookly/Features/home/presentation/views/home_view.dart';
+import 'package:bookly/Features/home/data/repos/home_repo_impl.dart';
+import 'package:bookly/Features/home/presentation/manger/featured_books_cubit/featured_books_cubit.dart';
 import 'package:bookly/core/utils/app_router.dart';
+import 'package:bookly/core/utils/service_locator.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'Features/home/presentation/manger/newest_books_cubit/newest_books_cubit.dart';
 import 'constants.dart';
-import 'Features/splash/presentation/views/splash_view.dart';
-import 'package:go_router/go_router.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
 void main() {
+  setupServiceLocator();
   runApp(
     DevicePreview(
       enabled: true,
-      builder: (context) => BooklyApp(), // Wrap your app
+      builder: (context) => const BooklyApp(), // Wrap your app
     ),
   );
 }
@@ -21,17 +24,28 @@ class BooklyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp.router(
-      useInheritedMediaQuery: true,
-      locale: DevicePreview.locale(context),
-      builder: DevicePreview.appBuilder,
-      routerConfig: AppRouter.router,
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: kPrimaryColor,
-       textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme)
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+            create: (context) => FeaturedBooksCubit(
+                  getIt.get<HomeRepoImpl>()
+                )),
+        BlocProvider(
+            create: (context) => NewestBooksCubit(
+              getIt.get<HomeRepoImpl>()
+            )),
+      ],
+      child: MaterialApp.router(
+        useInheritedMediaQuery: true,
+        locale: DevicePreview.locale(context),
+        builder: DevicePreview.appBuilder,
+        routerConfig: AppRouter.router,
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.dark().copyWith(
+            scaffoldBackgroundColor: kPrimaryColor,
+            textTheme:
+                GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme)),
       ),
-
     );
   }
 }
-
