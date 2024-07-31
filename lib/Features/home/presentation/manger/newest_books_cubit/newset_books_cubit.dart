fe47f370ecab260761c274e93b14f48ac84eb701
Ahraf01096 <ahmed.ashraf27163@gmail.com>
2024-07-31 09:59:30 +0300

    newest books cubit done

--- lib/Features/home/presentation/manger/newest_books_cubit/newset_books_cubit.dart
+++
@@ -1,8 +0,0 @@
-import 'package:bloc/bloc.dart';
-import 'package:equatable/equatable.dart';
-
-part 'newset_books_state.dart';
-
-class NewsetBooksCubit extends Cubit<NewsetBooksState> {
-  NewsetBooksCubit() : super(NewsetBooksInitial());
-}
